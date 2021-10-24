959972b242c75234c9d1d600d1eae11aba6fa83a
Htoo Maung Thait <htoomaungthait@gmail.com>
2021-10-24 23:22:06 +0630

    content container and heading created

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>Index of movies</div>
+
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
+                popular movies
+            </h2>
+        </div>
+    </div>
 
 
 @endsection

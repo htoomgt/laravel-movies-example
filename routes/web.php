91ef82dac8dcf556e65303dfd80a84f7b4213c4a
Htoo Maung Thait <htoomaungthait@gmail.com>
2021-11-10 23:58:03 +0630

    the movie db api called successfully

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movie/', [MoviesController::class, 'show'])->name('movies.show');
+Route::get('/movie/{id}', [MoviesController::class, 'show'])->name('movies.show');
+
+

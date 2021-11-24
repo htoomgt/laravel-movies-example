c2ecc7059f47215f1c95494690380a275be89310
Htoo Maung Thait <htoomaungthait@gmail.com>
2021-11-24 16:27:48 +0630

    completed part-5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -49,6 +49,7 @@
         </div>
     </nav>
     @yield('content')
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
     <livewire:scripts />
 
 </body>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -46,21 +46,47 @@
 
                 </div>
 
-                @if(count($movie['videos']['results']) > 0)
-
-
+                <div x-data="{isOpen: false}">
+                    @if(count($movie['videos']['results']) > 0)
                     <div class="mt-12">
-                        <a href="https://youtube.com/watch?v={{$movie['videos']['results'][0]['key']}}" class="inline-flex item-center bg-orange-500 text-gray-900  rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-                            </svg>
-
-                            <span class="ml-1">Play Trailler</span>
-                        </a>
+                        <button
+                            @click="isOpen = true"
+                            class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
+                        >
+                            <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                            <span class="ml-2">Play Trailer</span>
+                        </button>
                     </div>
 
-                @endif
+                    <div
+                        style="background-color: rgba(0, 0, 0, .5);"
+                        class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                        x-show.transition.opacity="isOpen"
+                    >
+                        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                            <div class="bg-gray-900 rounded">
+                                <div class="flex justify-end pr-4 pt-2">
+                                    <button
+                                        @click="isOpen = false"
+                                        @keydown.escape.window="isOpen = false"
+                                        class="text-3xl leading-none hover:text-gray-300">&times;
+                                    </button>
+                                </div>
+                                <div class="modal-body px-8 py-8">
+                                    <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                                        <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    @endif
+                </div>
+
+
 
 
             </div>
@@ -93,36 +119,68 @@
             </div>
         </div>
 
-        <div class="container mx-auto px-4 py-16">
-            <h2 class="text-4xl font-semibold">Image</h2>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-16">
+        <div class="movie-images" x-data="{ isOpen: false, image : ''}">
+            <div class="container mx-auto px-4 py-16">
+                <h2 class="text-4xl font-semibold">Image</h2>
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-16">
 
-                @foreach ($images as $image)
-                    <div class="mt-8">
-                        <a href="#">
-                            <img src="{{'https://image.tmdb.org/t/p/w500/'.$image['file_path']}}" alt="{{$image['file_path']}}" class="hover:opacity-75 transition ease-in-out duration-150" />
-                        </a>
+                    @foreach ($images as $image)
+                        <div class="mt-8">
+                            <a href="#">
+                                <a
+                                @click.prevent="
+                                    isOpen = true
+                                    image='{{ 'https://image.tmdb.org/t/p/original/'.$image['file_path'] }}'
+                                "
+                                href="#"
+                            >
+                                <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="image1" class="hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                        </div>
+
+                    @endforeach
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+
+            <div
+                style="background-color: rgba(0, 0, 0, .5);"
+                class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                x-show="isOpen"
+            >
+                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                    <div class="bg-gray-900 rounded">
+                        <div class="flex justify-end pr-4 pt-2">
+                            <button
+                                @click="isOpen = false"
+                                @keydown.escape.window="isOpen = false"
+                                class="text-3xl leading-none hover:text-gray-300">&times;
+                            </button>
+                        </div>
+                        <div class="modal-body px-8 py-8">
+                            <img :src="image" alt="poster">
+                        </div>
                     </div>
-
-                @endforeach
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                </div>
             </div>
         </div>
+
+
     </div>
 
 @endsection
